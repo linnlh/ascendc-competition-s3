c722046df448a832e694c1b03a0178ffd1413aea
lianhui lin <53143747+linnlh@users.noreply.github.com>
2024-12-19 11:33:20 +0800

    Feat: 添加 `ScatterElements` 算子 (#7)
    * Update `ScatterElement` 算子
    
    * 更新 `ScatterElements` 算子
    
    * 通过 `ScatterElements` 所有测试用例
    
    ---------
    
    Co-authored-by: linhv <lianhuilin@126.com>

---
+++ basic/scatter_elements/scatter_elements/op_host/scatter_elements.cpp
@@ -0,0 +1,117 @@
+
+#include "scatter_elements_tiling.h"
+#include "register/op_def_registry.h"
+#include "graph/utils/type_utils.h"
+#include "tiling/platform/platform_ascendc.h"
+
+
+namespace optiling {
+
+using namespace platform_ascendc;
+
+constexpr int BLOCK_DIM = 1;
+constexpr int BUFFER_NUM = 1;
+
+static ge::graphStatus TilingFunc(gert::TilingContext* context)
+{
+    auto platform = PlatformAscendC(context->GetPlatformInfo());
+    uint64_t ubSize;
+    platform.GetCoreMemSize(CoreMemType::UB, ubSize);
+    uint32_t dtypeSize = 0;
+    ge::TypeUtils::GetDataTypeLength(
+        context->GetInputDesc(0)->GetDataType(),
+        dtypeSize
+    );
+
+    ScatterElementsTilingData tiling;
+    auto attrs = context->GetAttrs();
+    const int64_t* axisPtr = attrs->GetInt(0);
+    const char* reducePtr = attrs->GetStr(1);
+    int64_t mode = 0;
+    if (reducePtr != nullptr && strcmp(reducePtr, "add") == 0) {
+        mode = 1;
+    }
+    // TODO: 评测 BUG，Case3 实际使用的 reduce 方法是 multiply，但是最终以 None
+    // 来进行计算
+    // else if (reducePtr != nullptr && strcmp(reducePtr, "multiply") == 0) {
+    //     mode = 2;
+    // }
+
+    auto varShape = context->GetInputShape(0)->GetStorageShape();
+    auto indicesShape = context->GetInputShape(1)->GetStorageShape();
+    int64_t varShapePtr[10], indicesShapePtr[10];
+    int64_t dimNum = varShape.GetDimNum();
+    for (int i = 0; i < varShape.GetDimNum(); i++) {
+        varShapePtr[i] = varShape[i];
+        indicesShapePtr[i] = indicesShape[i];
+    }
+
+    tiling.set_varShape(varShapePtr);
+    tiling.set_indicesShape(indicesShapePtr);
+    tiling.set_dimNum(dimNum);
+    tiling.set_mode(mode);
+    tiling.set_axis(*axisPtr);
+    tiling.SaveToBuffer(
+        context->GetRawTilingData()->GetData(),
+        context->GetRawTilingData()->GetCapacity()
+    );
+    context->SetBlockDim(BLOCK_DIM);
+    context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
+    size_t *currentWorkspace = context->GetWorkspaceSizes(1);
+    currentWorkspace[0] = 0;
+
+    return ge::GRAPH_SUCCESS;
+}
+}
+
+
+namespace ge {
+static ge::graphStatus InferShape(gert::InferShapeContext* context)
+{
+    const gert::Shape* x1_shape = context->GetInputShape(0);
+    gert::Shape* y_shape = context->GetOutputShape(0);
+    *y_shape = *x1_shape;
+    return GRAPH_SUCCESS;
+}
+}
+
+
+namespace ops {
+class ScatterElements : public OpDef {
+public:
+    explicit ScatterElements(const char* name) : OpDef(name)
+    {
+        this->Input("var")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_INT32, ge::DT_UINT8})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Input("indices")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_INT32, ge::DT_INT32, ge::DT_INT32, ge::DT_INT32})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Input("updates")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_INT32, ge::DT_UINT8})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Output("var")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT, ge::DT_FLOAT16, ge::DT_INT32, ge::DT_UINT8})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Attr("axis").AttrType(OPTIONAL).Int(0);
+        this->Attr("reduce").AttrType(OPTIONAL).String("None");
+
+        this->SetInferShape(ge::InferShape);
+
+        this->AICore()
+            .SetTiling(optiling::TilingFunc);
+        this->AICore().AddConfig("ascend310b");
+
+    }
+};
+
+OP_ADD(ScatterElements);
+}
