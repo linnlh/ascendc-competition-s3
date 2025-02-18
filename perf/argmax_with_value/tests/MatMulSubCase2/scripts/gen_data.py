442d0929be77113611812287a0058b1b2b5a9808
linlianhui <lianhuilin@126.com>
2025-02-19 00:44:35 +0800

    Feat: 添加官方文件

---
+++ perf/argmax_with_value/tests/MatMulSubCase2/scripts/gen_data.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+
+
+
+def gen_golden_data_simple():
+    os.system("mkdir -p input")
+    os.system("mkdir -p output")
+    input_x1 = np.random.uniform(-10, 10, [64,32]).astype(np.float32)
+    input_x2 = np.random.uniform(-10, 10, [32,1024]).astype(np.float32)
+    input_x3 = np.random.uniform(-10, 10, [64,1024]).astype(np.float32)
+
+    input_x1.tofile("./input/input_x1.bin")
+    input_x2.tofile("./input/input_x2.bin")
+    input_x3.tofile("./input/input_x3.bin")
+    golden = np.matmul(input_x1,input_x2) - input_x3
+
+    golden.tofile("./output/golden.bin")
+
+
+
+if __name__ == "__main__":
+    gen_golden_data_simple()
